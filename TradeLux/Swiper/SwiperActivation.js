25136e637454c9e9e2a2a21fc781c3866d1c1b40
Samuel Santos <48221347+iloading@users.noreply.github.com>
2019-05-17 18:34:08 +0100

    Ficheiros Iniciais
    Esqueleto das páginas de produto feitas com exemplo de swiper para as fotos do produto.

---
+++ TradeLux/Swiper/SwiperActivation.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
