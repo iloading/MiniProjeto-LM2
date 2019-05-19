fd80c2b0f59e9ffd1e13451d36fb9650817e8264
Samuel Santos <48221347+iloading@users.noreply.github.com>
2019-05-19 23:55:09 +0100

    Animação de scroll do header added

---
+++ TradeLux/JS/navHide.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("scroll").style.top = "0";
+  } else {
+    document.getElementById("scroll").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
