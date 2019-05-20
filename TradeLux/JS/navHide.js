3ab72d2577909f683d8b1c786ddef4574728cf6f
Samuel Santos <48221347+iloading@users.noreply.github.com>
2019-05-21 00:10:41 +0100

    Máquina de estados - bug não encontra a imagem

--- TradeLux/JS/navHide.js
+++ TradeLux/JS/navHide.js
@@ -1,3 +1,4 @@
+/*SCROLL NAV*/
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -7,4 +8,17 @@ window.onscroll = function() {
     document.getElementById("scroll").style.top = "-6rem";
   }
   prevScrollpos = currentScrollPos;
+}
+/*MÁQUINA DE ESTADOS*/
+
+var estado = 0;
+
+function slt() {
+  if (estado == 0) {
+    document.getElementById('fav').src='../TradeLux/Imagens/preenchido.png';
+    estado = 1;
+  } else {
+    document.getElementById('fav').src='../TradeLux/Imagens/aberto.png';
+    estado = 0;
+  }
 }
