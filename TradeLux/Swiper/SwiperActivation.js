5507b97bcf81eee578e13a8ccb75b3ca3d4d1998
Samuel Santos <48221347+iloading@users.noreply.github.com>
2019-05-22 14:48:53 +0100

    Swiper na Main page implementado - é preciso mudar as imagens

--- TradeLux/Swiper/SwiperActivation.js
+++ TradeLux/Swiper/SwiperActivation.js
@@ -1,10 +1,17 @@
 var swiper = new Swiper('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+     spaceBetween: 0,
+     centeredSlides: true,
+     autoplay: {
+       delay: 5000,
+       disableOnInteraction: false,
+     },
+     pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+     },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+
+   });
